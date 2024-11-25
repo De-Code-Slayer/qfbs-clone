1d694407ec67cb2b326db5b48647409ba15c4532
jacheme <chukwujapheth232@gmail.com>
2024-11-25 07:53:30 -0800

    .

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -37,7 +37,7 @@ def humanitarian_project(request):
     data = request.form
     converted_data = {key: str(value) for key, value in data.items()}
     print('SENDING humanitarian_project')
-    return send_mail(EMAIL_ADDRESS,json.dumps(converted_data),'humanitarian_project DOCS FROM QFBS', file=request.files)
+    return send_mail(EMAIL_ADDRESS,json.dumps(converted_data),f'Humanitarian Project DOCS FROM {current_user.email}', file=request.files.get('project'))
 
 def send_medbed(request):
     data = request.form
